051a0fa4ca248fa5ac281bcfac9947e17f2c54dd
andersone35 <61843712+andersone35@users.noreply.github.com>
2022-05-16 12:31:22 -0600

    Add files via upload

--- examples/example_yamlReader/example_sandwichPanel.py
+++ examples/example_yamlReader/example_sandwichPanel.py
@@ -5,10 +5,13 @@ import math
 import numpy as np
 from petsc4py import PETSc
 import os
+import sys
 import matplotlib.pyplot as plt
 
 from anba4 import *
-from ANBAInput import *  ## Experimental yaml reader
+
+sys.path.append("../../anba4")
+from ANBAInput import *
 
 ## Define input file name and read it to construct anba object
 inputFileName = 'sandwichPanel_simplified_triEls.yaml'
@@ -18,5 +21,14 @@ print('created anbax object')
 
 stiff = anba.compute()
 stiff.view()
+
+mass = anba.inertia()
+mass.view()
+
+anba.stress_field([0., 0., 1.,], [0., 0., 0.], "local", "paraview")
+anba.strain_field([0., 0., 1.,], [0., 0., 0.], "local", "paraview")
+
+outputFileName = 'sanwichPanel_output.yaml'
+writeResultYaml(anba,outputFileName)
 
 print('computed stiffness')
